fe173054451c6c994086022bc5d6545c3d1b9808
Senasn666 <54469886+sena606@users.noreply.github.com>
2023-02-19 16:37:15 +0700

    Add files via upload

---
+++ tugas1/1c.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas1c</title>
+<style>
+body {
+    margin:  0;
+    padding: 0;
+}
+    
+.kotak1 {
+    width:  80px;
+    height: 80px;
+    background-color: yellow;
+    border: 2px solid black;
+    text-align: center;
+    line-height: 80px;
+    font-size: 60px;
+}
+
+.kotak2 {
+    width:  80px;
+    height: 80px;
+    background-color: yellow;
+    border: 2px solid black;
+    text-align: center;
+    line-height: 80px;
+    font-size: 60px;
+    float: left;
+}
+
+.kotak3 {
+    width:  80px;
+    height: 80px;
+    background-color: yellow;
+    border: 2px solid black;
+    text-align: center;
+    line-height: 80px;
+    font-size: 60px;
+    float: left;
+}
+.clear {
+    clear: left;
+}
+
+</style>
+</head>
+</body>
+    <div class="kotak1">1</div>
+    <div class="kotak2">2</div><div class="kotak2">2</div><div class="clear"></div>
+    <div class="kotak3">3</div><div class="kotak3">3</div><div class="kotak3">3</div>
+
+</body>
+</html>
